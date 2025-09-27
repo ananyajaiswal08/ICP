43a0db6041cafc8b68ff4a168e76c0b3e82ff9e9
Ananya Jaiwal <ananyajaiswal2315@gmail.com>
2025-09-27 15:53:58 +0530

    find first and last elements

--- Sep26/findfirstlast.java
+++ Sep26/findfirstlast.java
@@ -1,42 +1,43 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int firstPos = findBoundary(nums, target, true);
-        if (firstPos == -1) {
-            return new int[]{-1, -1};
-        }
-        int lastPos = findBoundary(nums, target, false);
-        
-        return new int[]{firstPos, lastPos};
-    }
-    private int findBoundary(int[] nums, int target, boolean findFirst) {
-        int low = 0;
-        int high = nums.length - 1;
-        int index = -1; // Stores the potential result
-        
-        while (low <= high) {
-            // Using low + (high - low) / 2 prevents potential integer overflow
-            int mid = low + (high - low) / 2;
-            
-            if (nums[mid] == target) {
-                index = mid;
-                if (findFirst) {
-                    // To find the FIRST occurrence, keep searching in the LEFT half
-                    high = mid - 1;
-                } else {
-                    // To find the LAST occurrence, keep searching in the RIGHT half
-                    low = mid + 1;
-                }
-            } else if (nums[mid] < target) {
-                // Target must be in the right half
-                low = mid + 1;
-            } else { // nums[mid] > target
-                // Target must be in the left half
-                high = mid - 1;
+    public int lower(int[] nums, int n, int t) {
+        int s=0, e=n-1;
+        int l=-1;
+        while(s<=e) {
+            int m = s + (e-s)/2;
+            if(nums[m]==t) {
+                l=m;
+                e=m-1;
+            }else if(nums[m] > t) {
+                e=m-1;
+            }else {
+                s=m+1;
             }
         }
-        
-        return index;
+        return l;
+    }
+    public int higher(int[] nums, int n, int t) {
+        int s=0, e=n-1;
+        int h=-1;
+        while(s<=e) {
+            int m = s + (e-s)/2;
+            if(nums[m]==t) {
+                h=m;
+                s=m+1;
+            }else if(nums[m] > t) {
+                e=m-1;
+            }else {
+                s=m+1;
+            }
+        }
+        return h;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int l = lower(nums, n, target);
+        int h = higher(nums, n, target);
+        int[] arr = new int[2];
+        arr[0] = l;
+        arr[1] = h;
+        return arr;
     }
 }
-        
-    
